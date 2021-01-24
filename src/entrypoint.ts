28d379de8415088fa3942969efedf10b5415ad5f
wspwebben <wspwebben@gmail.com>
2021-01-25 02:23:41 +0700

    remove redundant files

--- src/entrypoint.ts
+++
@@ -1,9 +0,0 @@
-import './assets/style/index.scss';
-
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router';
-
-createApp(App)
-  .use(router)
-  .mount('#app')
