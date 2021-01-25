04a283b70935e34ab9053d40f63b3a551b8522d1
wspwebben <wspwebben@gmail.com>
2021-01-26 01:26:23 +0700

    add ant-design globally (temp)

--- src/entry.js
+++ src/entry.js
@@ -1,6 +1,11 @@
 import './assets/style/index.scss';
+import 'ant-design-vue/dist/antd.css';
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import Antd from 'ant-design-vue';
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(Antd)
+  .mount('#app')
+
