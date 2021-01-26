604bba7dd0a953beb3c91f4296883a9e56fa0741
wspwebben <wspwebben@gmail.com>
2021-01-27 03:55:25 +0700

    add router

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,5 @@
 </script>
 
 <template>
-  <div>Hello world</div>
+  <div><router-view /></div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/entry.js
+++ src/entry.js
@@ -2,10 +2,12 @@ import './assets/style/index.scss';
 import 'ant-design-vue/dist/antd.css';
 
 import { createApp } from 'vue'
-import App from './App.vue'
 import Antd from 'ant-design-vue';
+import { router } from '/@/router';
+import App from './App.vue'
 
 createApp(App)
+  .use(router)
   .use(Antd)
   .mount('#app')
 
