92dacf8a3694dbd0aa714b7fcfa9f486774b2bd0
wspwebben <wspwebben@gmail.com>
2021-01-28 03:04:28 +0700

    add wrapper for grid and card components

---
+++ src/Base/components/Card.vue
@@ -0,0 +1,16 @@
+<script>
+import { Card as AntCard } from 'ant-design-vue';
+
+export default {
+  name: 'BaseCard',
+  components: {
+    AntCard
+  },
+}
+</script>
+
+<template>
+  <AntCard v-bind="$attrs">
+    <slot />
+  </AntCard>
+</template>
---
+++ src/Base/components/Grid.vue
@@ -0,0 +1,29 @@
+<script>
+import { List as AntList } from 'ant-design-vue';
+
+const { Item: AntListItem } = AntList;
+
+export default {
+  name: 'BaseGrid',
+  components: {
+    AntList,
+    AntListItem,
+  },
+  props: {
+    items: {
+      type: Array,
+      default: () => [],
+    }
+  }
+}
+</script>
+
+<template>
+  <AntList :grid="{ gutter: 20, column: 4 }" :data-source="items">
+    <template #renderItem="{ item }">
+      <AntListItem>
+        <slot :item="item" />
+      </AntListItem>
+    </template>
+  </AntList>
+</template>
