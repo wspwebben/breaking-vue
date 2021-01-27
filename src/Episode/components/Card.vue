3af74660f8237a28f000991b20b8c871f984facc
wspwebben <wspwebben@gmail.com>
2021-01-28 03:09:00 +0700

    add episode cards list

---
+++ src/Episode/components/Card.vue
@@ -0,0 +1,41 @@
+<script>
+import BaseCard from '/@/Base/components/Card.vue';
+
+export default {
+  name: 'EpisodeCard',
+  components: {
+    BaseCard
+  },
+  props: {
+    id: {
+      type: Number,
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    characters: {
+      type: Array,
+      default: () => [],
+    },
+  }
+}
+</script>
+
+<template>
+  <BaseCard class="EpisodeCard">
+    <h3 class="EpisodeCard__title">{{ title }}</h3>
+
+    <ul class="EpisodeCard__character-list">
+      <li class="EpisodeCard__character" v-for="character in characters" :key="character">{{ character }}</li>
+    </ul>
+  </BaseCard>
+</template>
+
+<style lang="scss">
+.EpisodeCard {
+
+  &__character-list {
+  }
+}
+</style>
--- src/Episode/views/List.vue
+++ src/Episode/views/List.vue
@@ -1,8 +1,53 @@
 <script>
+import { ref } from 'vue'; 
+
+import BaseGrid from '/@/Base/components/Grid.vue';
+import EpisodeCard from '/@/Episode/components/Card.vue';
+
+import { getAllEpisodes } from '/@/Episode/api';
+
+export default {
+  name: 'ViewEpisodeList',
+  components: {
+    BaseGrid,
+    EpisodeCard,
+  },
+  setup() {
+    const episodes = ref([]);
+
+    getAllEpisodes().then(list => {
+      episodes.value = list;
+    });
+
+    return {
+      episodes
+    }
+  }
+}
 </script>
 
 <template>
   <div class="ViewEpisodeList container">
-    Hello list
+    <BaseGrid :items="episodes">
+      <template #default="{ item: episode }">
+        <EpisodeCard
+          :id="episode.id"
+          :title="episode.title"
+          :characters="episode.characters"
+        />
+      </template>
+    </BaseGrid>
   </div>
 </template>
+
+<style lang="scss">
+.ViewEpisodeList {
+  padding-top: 20px;
+
+  &__list {
+    display: grid;
+    gap: 20px;
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  }
+}
+</style>
